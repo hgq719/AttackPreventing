489c122bce29ad2e827339784c8e5cbb146f3650
joy_chen@comm100.com <joy_chen@comm100.com>
2018-11-15 10:08:20 +0800

    Add project files.

---
+++ AttackPrevent/Scripts/layout.js
@@ -0,0 +1,11 @@
+﻿
+$(document).ready(function () {
+    $(".headerRight").hover(function () {
+        console.log("aaaa");
+        $(this).children(".divAvatarsHasTop").stop(false, true).slideDown(300);
+    }, function () {
+        $(this).children(".divAvatarsHasTop").stop(false, true).css("display", "none");
+    }
+    );
+});
+

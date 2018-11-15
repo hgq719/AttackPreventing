44a3a4e978a1df0398d318da7a89f36301ac1780
DamoOnly <307830062@qq.com>
2018-11-15 16:00:37 +0800

    add logs page

--- AttackPrevent/Controllers/HomeController.cs
+++ AttackPrevent/Controllers/HomeController.cs
@@ -26,5 +26,10 @@ namespace AttackPrevent.Controllers
 
             return View();
         }
+
+        public ActionResult CloundflareDownloadLogs()
+        {
+            return View();
+        }
     }
 }

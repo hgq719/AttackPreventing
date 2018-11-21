193545c3dae0a1f1bce25daf80eebbda37b8b31a
DamoOnly <307830062@qq.com>
2018-11-21 10:20:04 +0800

    update

--- AttackPrevent/Controllers/HomeController.cs
+++ AttackPrevent/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using AttackPrevent.Model;
+﻿using AttackPrevent.Business;
+using AttackPrevent.Model;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -83,8 +84,14 @@ namespace AttackPrevent.Controllers
 
         public ActionResult ZoneList()
         {
+            return new HttpUnauthorizedResult();
             return View();
         }
+
+        //private void ck()
+        //{
+        //    throw new HttpUnauthorizedResult();
+        //}
 
         public ActionResult AddZone()
         {
@@ -92,63 +99,15 @@ namespace AttackPrevent.Controllers
         }
 
         public JsonResult GetAuditLog(int limit, int offset, string zoneID, DateTime startTime, DateTime endTime, string logType, string detail)
-        {
-            List<AuditLogEntity> list = new List<AuditLogEntity>();
-            for (int i = 0; i < 50; i++)
-            {
-                AuditLogEntity en = new AuditLogEntity();
-                en.ID = i;
-                en.LogType = "App";
-                en.Detail = "detail" + i;
-                en.LogTime = DateTime.Now;
-                en.LogOperator = "Michael.he";
+        {   
+            dynamic result = AuditLogBusiness.GetAuditLog(limit, offset, zoneID, startTime, endTime, logType, detail);
 
-                list.Add(en);
-            }
-
-            var total = list.Count;
-            var rows = list.Skip(offset).Take(limit).ToList();
-            return Json(new { total = total, rows = rows }, JsonRequestBehavior.AllowGet); 
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
 
         public FileResult ExportAuditLog(string zoneID, DateTime startTime, DateTime endTime, string logType, string detail) 
         {
-            //创建Excel文件的对象
-            NPOI.HSSF.UserModel.HSSFWorkbook book = new NPOI.HSSF.UserModel.HSSFWorkbook();
-            //添加一个sheet
-            NPOI.SS.UserModel.ISheet sheet1 = book.CreateSheet("Sheet1");
-            //获取list数据
-            List<AuditLogEntity> list = new List<AuditLogEntity>();
-            for (int i = 0; i < 50; i++)
-            {
-                AuditLogEntity en = new AuditLogEntity();
-                en.ID = i;
-                en.LogType = "App";
-                en.Detail = "detail" + i;
-                en.LogTime = DateTime.Now;
-                en.LogOperator = "Michael.he";
-
-                list.Add(en);
-            }
-            //给sheet1添加第一行的头部标题
-            NPOI.SS.UserModel.IRow row1 = sheet1.CreateRow(0);
-            row1.CreateCell(0).SetCellValue("Log Type");
-            row1.CreateCell(1).SetCellValue("Detail");
-            row1.CreateCell(2).SetCellValue("Log Time");
-            row1.CreateCell(3).SetCellValue("Log Operator");
-            //将数据逐步写入sheet1各个行
-            for (int i = 0; i < list.Count; i++)
-            {
-                NPOI.SS.UserModel.IRow rowtemp = sheet1.CreateRow(i + 1);
-                rowtemp.CreateCell(0).SetCellValue(list[i].LogType);
-                rowtemp.CreateCell(1).SetCellValue(list[i].Detail);
-                rowtemp.CreateCell(2).SetCellValue(list[i].LogTime.ToString());
-                rowtemp.CreateCell(3).SetCellValue(list[i].LogOperator);
-            }
-            // 写入到客户端 
-            System.IO.MemoryStream ms = new System.IO.MemoryStream();
-            book.Write(ms);
-            ms.Seek(0, SeekOrigin.Begin);
+            MemoryStream ms = AuditLogBusiness.ExportAuditLog(zoneID, startTime, endTime, logType, detail);
             return File(ms, "application/vnd.ms-excel", "AuditLog.xls");
         }
     }

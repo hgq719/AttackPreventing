38b75af6512660003e4e223fcea24702a1169ccc
page <page@comm100.com>
2018-11-30 11:03:08 +0800

    check cloundflare auth

--- AttackPrevent/App_Start/BaseController.cs
+++ AttackPrevent/App_Start/BaseController.cs
@@ -40,5 +40,32 @@ namespace AttackPrevent
 #endif
             }
         }
+        public JsonResult CheckCloundflareAuth(string zoneID)
+        {
+            string authEmail = "";
+            string authKey = "";
+            //zoneID = "";
+
+            var zoneList = ZoneBusiness.GetZoneList();
+            var zone = zoneList.FirstOrDefault(a => a.ZoneId == zoneID);
+            authEmail = zone.AuthEmail;
+            authKey = zone.AuthKey;
+
+            bool isSuccessed = false;
+            string errorMsg = "";
+
+            ICloundFlareApiService cloundFlareApiService = new CloundFlareApiService();
+            string strResult = cloundFlareApiService.CheckAuth(zoneID, authEmail, authKey);
+            if (!string.IsNullOrEmpty(strResult))
+            {
+                errorMsg = string.Format("Cloundflare {0}", strResult) ;
+            }
+            else
+            {
+                isSuccessed = true;
+            }
+
+            return Json(new { isSuccessed, errorMsg = errorMsg }, JsonRequestBehavior.AllowGet);
+        }
     }
 }

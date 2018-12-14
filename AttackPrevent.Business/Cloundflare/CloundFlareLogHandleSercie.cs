a7e69329824306c2ccc4ca017e46563541844c77
page <page@comm100.com>
2018-12-14 15:35:40 +0800

    fix get cloundflare logs delete try catch

--- AttackPrevent.Business/Cloundflare/CloundFlareLogHandleSercie.cs
+++ AttackPrevent.Business/Cloundflare/CloundFlareLogHandleSercie.cs
@@ -35,9 +35,6 @@ namespace AttackPrevent.Business
         private string authEmail;
         private string authKey;
 
-        private object obj_Sync = new object();
-        private CloudflareLogHandleSercie cloudflareLogHandleSercie;
-
         public CloudflareLogHandleSercie(string zoneId, string authEmail, string authKey, double sample, DateTime start, DateTime end)
         {
             this.zoneId = zoneId;
@@ -49,18 +46,6 @@ namespace AttackPrevent.Business
 
             cloundFlareApiService = new CloundFlareApiService();
             logService = new Business.LogService();
-        }
-        private CloudflareLogHandleSercie GetInstance()
-        {
-            if (cloudflareLogHandleSercie == null)
-            {
-                lock (obj_Sync)
-                {
-                    //cloudflareLogHandleSercie = new CloudflareLogHandleSercie();
-                }
-            }
-
-            return cloudflareLogHandleSercie;
         }
 
         private ICloundFlareApiService cloundFlareApiService;
@@ -91,51 +76,44 @@ namespace AttackPrevent.Business
         //取队列数据处理
         public void Dequeue()
         {
-            try
+            while (true)
             {
-                while (true)
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                KeyValuePair<DateTime, DateTime> keyValuePair = default(KeyValuePair<DateTime, DateTime>);
+                if (keyValuePairs.TryDequeue(out keyValuePair))
                 {
-                    Stopwatch stopwatch = new Stopwatch();
-                    stopwatch.Start();
-                    KeyValuePair<DateTime, DateTime> keyValuePair = default(KeyValuePair<DateTime, DateTime>);
-                    if (keyValuePairs.TryDequeue(out keyValuePair))
+                    DateTime start = keyValuePair.Key;
+                    DateTime end = keyValuePair.Value;
+
+                    string time = string.Format("{0}-{1}", start.ToString("yyyyMMddHHmmss"), end.ToString("yyyyMMddHHmmss"));
+                    bool retry = false;
+                    List<CloudflareLog> cloudflareLogs = cloundFlareApiService.GetCloudflareLogs(zoneId, authEmail, authKey, sample, start, end, out retry);
+                    while (retry == true)
                     {
-                        DateTime start = keyValuePair.Key;
-                        DateTime end = keyValuePair.Value;
-
-                        string time = string.Format("{0}-{1}", start.ToString("yyyyMMddHHmmss"), end.ToString("yyyyMMddHHmmss"));
-                        bool retry = false;
-                        List<CloudflareLog> cloudflareLogs = cloundFlareApiService.GetCloudflareLogs(zoneId, authEmail, authKey, sample, start, end, out retry);
-                        while (retry == true)
-                        {                            
-                            cloudflareLogs = cloundFlareApiService.GetCloudflareLogs(zoneId, authEmail, authKey, sample, start, end, out retry);
-                        }
-
-                        string key = string.Format("{0}-{1}-{2}-{3}", startTime.ToString("yyyyMMddHHmmss"), endTime.ToString("yyyyMMddHHmmss"), sample, zoneId);
-                        if (!dicCloudflareLogs.Keys.Contains(key))
-                        {
-                            dicCloudflareLogs.TryAdd(key, new List<CloudflareLog>());
-                        }
-
-                        if (cloudflareLogs != null && cloudflareLogs.Count > 0)
-                        {                            
-                            dicCloudflareLogs[key].AddRange(cloudflareLogs);
-                        }
-
-                        stopwatch.Stop();                     
-                        
+                        cloudflareLogs = cloundFlareApiService.GetCloudflareLogs(zoneId, authEmail, authKey, sample, start, end, out retry);
                     }
-                    else
+
+                    string key = string.Format("{0}-{1}-{2}-{3}", startTime.ToString("yyyyMMddHHmmss"), endTime.ToString("yyyyMMddHHmmss"), sample, zoneId);
+                    if (!dicCloudflareLogs.Keys.Contains(key))
                     {
-                        break;
+                        dicCloudflareLogs.TryAdd(key, new List<CloudflareLog>());
                     }
+
+                    if (cloudflareLogs != null && cloudflareLogs.Count > 0)
+                    {
+                        dicCloudflareLogs[key].AddRange(cloudflareLogs);
+                    }
+
+                    stopwatch.Stop();
+
+                }
+                else
+                {
+                    break;
                 }
             }
-            catch(Exception e)
-            {
-                logService.Error(e.Message);
-            }
-           
+
         }
         //开启任务
         public void TaskStart()

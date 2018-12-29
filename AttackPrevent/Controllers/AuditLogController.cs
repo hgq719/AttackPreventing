4a60af7bab28ea75084864af1324ba3f2200a79b
DamoOnly <307830062@qq.com>
2018-12-29 10:34:49 +0800

    update maxlength

--- AttackPrevent/Controllers/AuditLogController.cs
+++ AttackPrevent/Controllers/AuditLogController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.IO;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace AttackPrevent.Controllers
 {
@@ -16,11 +17,15 @@ namespace AttackPrevent.Controllers
 
         public JsonResult GetAuditLog(int limit, int offset, int zoneTableID, DateTime? startTime, DateTime? endTime, string logType, string detail, bool ifUseCache)
         {
-
             //dynamic result = AuditLogBusiness.GetAuditLog(limit, offset, zoneTableID, startTime, endTime, logType, detail, ifUseCache, UserName);
             dynamic result = AuditLogBusiness.GetAuditLogByPage(limit, offset, zoneTableID, startTime, endTime, logType, detail);
-
-            return Json(result, JsonRequestBehavior.AllowGet);
+            return new JsonResult()
+            {
+                Data = result,
+                MaxJsonLength = Int32.MaxValue,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+            //return Json(result, JsonRequestBehavior.AllowGet);
         }
 
         public FileResult ExportAuditLog(int zoneTableID, DateTime? startTime, DateTime? endTime, string logType, string detail)

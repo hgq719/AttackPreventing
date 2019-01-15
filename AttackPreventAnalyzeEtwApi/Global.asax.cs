fc593da0db0fd32da360deb2292effcb7c6ab473
page <page@comm100.com>
2019-01-15 18:21:34 +0800

    iis log 2

--- AttackPreventAnalyzeEtwApi/Global.asax.cs
+++ AttackPreventAnalyzeEtwApi/Global.asax.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Quartz;
+using Quartz.Impl;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -13,11 +16,36 @@ namespace AttackPreventAnalyzeEtwApi
     {
         protected void Application_Start()
         {
+            //配置log4
+            log4net.Config.XmlConfigurator.Configure(new System.IO.FileInfo(Server.MapPath("~/Web.config")));
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            // 使api返回为json 
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+            //MapperInitialize.Initialize();
+            RunProgram().GetAwaiter();
         }
+
+        #region Start Quartz
+        async Task RunProgram()
+        {
+            try
+            {
+                StdSchedulerFactory factory = new StdSchedulerFactory();
+                IScheduler scheduler = await factory.GetScheduler();
+
+                // and start it off
+                await scheduler.Start();
+            }
+            catch (Exception e)
+            {
+                await Console.Error.WriteLineAsync(e.ToString());
+            }
+        }
+        #endregion
     }
 }
